eee0dba05b71d1ca39d6e71fe89320df0fdb48c6
prusotamydv <prusotamkumaryadav78@gmail.com>
2025-04-21 15:41:19 +0545

    schema are create, models for the project

---
+++ models/User.model.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      trim: true,
+      type: String,
+      required: true,
+    },
+    email: {
+      trim: true,
+      type: String,
+      required: true,
+    },
+    password: {
+      trim: true,
+      type: String,
+      required: true,
+    },
+    bio: {
+      trim: true,
+      type: String,
+      required: false,
+    },
+    profileImage: {
+      type: String,
+      public_id: String,
+    },
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", UserSchema);
